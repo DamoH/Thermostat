b75340aa8f7d47c1708806e5a9405af6483941de
Vu Le <core.safecrlf>
2018-10-03 14:40:49 +0100

    it has a up function

--- jasmine-standalone-3.2.1/spec/ThermostatSpec.js
+++ jasmine-standalone-3.2.1/spec/ThermostatSpec.js
@@ -1,6 +1,16 @@
 describe ('Thermostat', function () {
+  var thermostat;
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
   it ('the termostat starts at 20 degrees', function () {
-    var thermostat = new Thermostat;
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it ('has an increase function', function() {
+    // console.log(thermostat)
+    thermostat.up(2)
+    expect(thermostat.temperature).toEqual(22);
+  });
 });
+
--- jasmine-standalone-3.2.1/src/Thermostat.js
+++ jasmine-standalone-3.2.1/src/Thermostat.js
@@ -1,3 +1,7 @@
-var Thermostat = function () {
+function Thermostat() {
   this.temperature = 20;
+}
+
+Thermostat.prototype.up = function(number) {
+  this.temperature += number;
 };
